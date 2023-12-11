40b0748f2b5c470ca512c180268576931429932e
Shohei <shohei.cajasw@gmail.com>
2023-12-11 00:53:48 -0800

    Shohei Second Updated Dec 11 2023
    Downloaded 2 free resources & created scripts for movement and camera

--- Assets/Scripts/Shohei/Shohei_PlayerController.cs
+++ Assets/Scripts/Shohei/Shohei_PlayerController.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Shohei_PlayerController: MonoBehaviour
 {
-    [SerializeField] private float jumpForce = 4f;
-    [SerializeField] private float speed = 3f;
-
+    [SerializeField] private float jumpForce = 6f;
+    [SerializeField] private float playerSpeed = 3f;
     [SerializeField] private Rigidbody2D player;
+
+    public SpriteRenderer playerRenderer;
+    public float inputPlayer;
+
+    public LayerMask groundLayer;
+    private bool isOnGround;
+    public Transform groundPosition;
+    public float groundCheckCircle;
+
+    public float jumpTime = 0.5f;
+    public float jumpTimecounter;
+    private bool isJumpting;
+
+
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
@@ -16,14 +30,60 @@ public class Shohei_PlayerController: MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Move charactor left and right
         float horizontalInput = Input.GetAxis("Horizontal");
-        player.velocity = new Vector2(horizontalInput * speed, player.velocity.y);
+        player.velocity = new Vector2(horizontalInput * playerSpeed, player.velocity.y);
 
-        float verticalInput = Input.GetAxis("Vertical");
 
-        if (Input.GetKeyDown(KeyCode.UpArrow)) 
+        // Flip the face of charactor 
+        inputPlayer = Input.GetAxisRaw("Horizontal");
+        if (inputPlayer < 0)
         {
-            player.AddForce(player.transform.position * jumpForce, ForceMode2D.Impulse);
+            playerRenderer.flipX = true;
+        } else if (inputPlayer > 0)
+        {
+            playerRenderer.flipX = false;
+        }
+
+        isOnGround = Physics2D.OverlapCircle(groundPosition.position, groundCheckCircle, groundLayer);
+
+        if (Input.GetButtonDown("Jump") && isOnGround ==true)
+        {
+            isJumpting = true;
+            jumpTimecounter = jumpTime;
+            player.velocity = Vector2.up * jumpForce;
+            //player.AddForce(player.transform.position * jumpForce, ForceMode2D.Impulse);
+        }         
+
+        if (Input.GetButton("Jump") && isJumpting == true)
+        {
+            if(jumpTimecounter > 0) 
+            {
+                player.velocity = Vector2.up * jumpForce;
+                jumpTimecounter -= Time.deltaTime;
+            }
+            else 
+            {
+                isJumpting = false;
+            }
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumpting = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(player);
+        }
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            bool groundCheck = true;
         }
     }
 }
